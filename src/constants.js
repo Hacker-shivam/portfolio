d30fa3d68438586ecd180acbd8da7be3b26c8ce1
Hacker-shivam <saurabhsingh847101@gmail.com>
2026-03-17 23:33:46 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,282 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import simplify from './assets/company_logo/simplify.png';
+import edu from './assets/company_logo/edu.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import mdu from './assets/education_logo/mdu.png';
+import bseb from './assets/education_logo/bseb.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: simplify,
+      role: "Software Engineer",
+      company: "Simplify 3x pvt.ltd",
+      date: "AUG 2025 - FEB 2026",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: edu,
+      role: "MERN Trainee",
+      company: " Edunet Foundation",
+      date: "Jan 2024 - Dec 2024",
+      desc: "Trained in MERN stack development, building responsive UIs with React and integrating REST APIs using Express.js & MongoDB . Gained hands-on experience in full-stack workflows, authentication basics, and deployment practices.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: mdu,
+      school: "Maharshi Dayanand University",
+      date: "Sept 2022 - July 2026",
+      grade: "7.81 CGPA",
+      desc: "I have Pursuing my Bachelor degree (B>TECH) in Computer Science & Engineering from MD University, Rohtak. During my time at MDU, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor Of Technology - B.TECH",
+    },
+    {
+      id: 1,
+      img: bseb,
+      school: "BSEB, DARBHANGA",
+      date: "April 2020 - Apr 2022",
+      grade: "70%",
+      desc: "I completed my class 12 education from BSEB, DARBHANGA, under the BSEB board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "BSEB(XII) - PCM with Computer Science",
+    },
+    
+    {
+      id: 2,
+      img: bseb,
+      school: "BSEB, DARBHANGA, BIHAR",
+      date: "Apr 2019 - March 2020",
+      grade: "72%",
+      desc: "I completed my class 10 education from BSEB DARBHANGA, BIHAR, under the BSEB board, where I studied Science with Computer.",
+      degree: "BSEB(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "BHUMI",
+      description:
+        "Agri-Ai is an AI-powered web application designed to assist farmers and agricultural stakeholders by recommending optimal crops and fertilizers based on environmental parameters and location. Leveraging machine learning models and a simple web interface, the system aims to provide actionable insights for improved agricultural decision-making.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript"," PYTHON", "React JS", "API", "Express.js", "Authentication", "Flask-API"],
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 1,
+      title: "MedEasy",
+      description:
+        "MedEasy is a full-stack healthcare web application that simplifies medical service booking, doctor discovery, and secure online payments. It provides a modern, scalable solution for patients, doctors, and admins using industry-grade tools and APIs.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Hacker-shivam/MedEasy",
+      webapp: "",
+    },
+    {
+      id: 2,
+      title: "Task Management",
+      description:
+        "A simple task management web application built with React and Vite — designed to help users create, view, and manage tasks efficiently.",
+      image: movierecLogo,
+      tags: ["React JS", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Hacker-shivam/Task-management",
+      webapp: "https://emager.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Ai-Doc-Assistant",
+      description:
+        "An AI Document Assistant is a smart software tool that uses artificial intelligence to help users create, edit, organize, and analyze documents efficiently. It can automatically generate content, summarize long texts, check grammar, suggest improvements, and even extract key information from files like PDFs or Word documents.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/Hacker-shivam/Ai-Doc-Assistant",
+      webapp: "",
+    },
+    {
+      id: 4,
+      title: "Data-Analyst-Chatbot",
+      description:
+        "A Data Analyst Chatbot is an AI-powered tool that helps users analyze data through simple conversations. Instead of writing complex queries or code, users can ask questions in natural language, and the chatbot provides insights, summaries, or visualizations.",
+      image: taskremLogo,
+      tags: ["Python", "HTML", "CSS"],
+      github: "https://github.com/Hacker-shivam/Data-Analyst-Chatbot",
+      webapp: "",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Code Reviewer",
+      description:
+        "An interactive full‑stack application that allows users to paste code and receive concise, structured AI code reviews — covering errors, suggestions, optimization, and refactoring.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express",],
+      github: "",
+      webapp: "",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/Hacker-shivam/code-reviewer",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
